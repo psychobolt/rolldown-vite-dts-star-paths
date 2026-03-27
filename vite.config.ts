c3d60dbf3493aef16ee2087eaf032f723cc53455
psychobolt <560721+psychobolt@users.noreply.github.com>
2026-03-27 14:08:02 -0700

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import { dts } from 'rolldown-plugin-dts';
 
 export default defineConfig({
-  plugins: [dts()],
   build: {
     lib: {
       entry: {
@@ -11,18 +10,19 @@ export default defineConfig({
         'Header/index': 'src/Header/index.ts',
       },
       formats: ['es'],
-    }
-  },
-  rolldownOptions: {
-    output: {
-      codeSplitting: {
-        groups: [
-          {
-            name(id) {
-              return id.replace(/^(.*src)[/\\]/, '').replace('.ts', '');
+    },
+    rolldownOptions: {
+      plugins: [dts()],
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name(id) {
+                return id.replace(/^(.*src)[/\\]/, '').replace('.ts', '');
+              },
             },
-          },
-        ],
+          ],
+        }
       }
     }
   }
